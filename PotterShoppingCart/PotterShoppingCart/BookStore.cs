578715b43ad085b6590500b7d3f26b7a9c0bd1ae
VisualStudy <vigor_han@hotmail.com>
2016-10-21 15:22:29 +0800

    First TestCase Commit1

---
+++ PotterShoppingCart/PotterShoppingCart/BookStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart
+{
+    public class BookStore
+    {
+        public BookStore(IList<ShoppingCart> Books)
+        {
+                
+        }
+
+        public double CountTotalPay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ShoppingCart
+    {
+        public string Book { get; set; }
+    }
+}
---
+++ PotterShoppingCart/PotterShoppingCartTests/BookStoreTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass()]
+    public class BookStoreTests
+    {
+        [TestMethod()]
+        public void Test_ShoppingCart_Buy_1Book_Should_Pay_100()
+        {
+            List<ShoppingCart> shoppingCart = new List<ShoppingCart>
+            {
+                new ShoppingCart {Book="Book1" }
+            };
+
+            var target = new BookStore(shoppingCart);
+            
+            double Actual = target.CountTotalPay();
+            double expected = 100;
+
+            Assert.AreEqual(expected,100);
+        }
+    }
+}
